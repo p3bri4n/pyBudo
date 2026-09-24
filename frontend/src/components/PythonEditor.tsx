c54b5e55a9477405696d6c8b6afc2ed51102219a
Aksel <axel.akondegmail.com>
2026-09-24 20:03:51 +0200

    Refs #30 feat(react): add common-style folder

--- frontend/src/components/PythonEditor.tsx
+++ frontend/src/components/PythonEditor.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { executePython } from "../services/pyodide/PyodideService";
 import type {ExecutionResult} from "../interfaces/interfaces.ts";
+import "./python-editor.css"
+import {useTranslation} from "react-i18next";
 
 export function PythonRunner() {
     const [code, setCode] = useState("print('Hello pyBudo!')");
     const [output, setOutput] = useState("");
     const [loading, setLoading] = useState(false);
+    const {t} = useTranslation();
 
     async function runCode(): Promise<void> {
         setLoading(true);
@@ -26,14 +29,13 @@ export function PythonRunner() {
     }
 
     return (
-        <div>
-      <textarea
-          value={code}
-          onChange={(event) => setCode(event.target.value)}
-      />
-
+        <div className={"pythonEditor"}>
+            <textarea
+                value={code}
+                onChange={(event) => setCode(event.target.value)}
+            />
             <button onClick={runCode} disabled={loading}>
-                {loading ? "Exécution..." : "Exécuter"}
+                {loading ? `${t("python-runner.execution")}` : `${t("python-runner.execute")}`}
             </button>
 
             <pre>{output}</pre>
